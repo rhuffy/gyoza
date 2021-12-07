1a402ab69607d44076aef81ff5ac4472d2f994a9
Sidharth Menon <menon.sid.k@gmail.com>
2021-12-07 16:55:53 -0500

    [instance featurizer] switch to __call__

--- models/instance_featurizer.py
+++ models/instance_featurizer.py
@@ -6,12 +6,12 @@ import numpy as np
 
 class InstanceFeaturizer(ABC):
     @abstractmethod
-    def featurize(self, node_yaml_str: str) -> np.ndarray:
+    def __call__(self, node_yaml_str: str) -> np.ndarray:
         pass
 
 
-class DefaultNodeFeaturizer(InstanceFeaturizer):
-    def featurize(self, node_yaml_str: str) -> np.ndarray:
+class DefaultInstanceFeaturizer(InstanceFeaturizer):
+    def __call__(self, node_yaml_str: str) -> np.ndarray:
         try:
             node_yaml = yaml.safe_load(node_yaml_str)
             # TODO: actually generate docker_compose files appropriately here, etc.
@@ -22,5 +22,5 @@ class DefaultNodeFeaturizer(InstanceFeaturizer):
             ]
             return np.array(embeddings)
         except yaml.YAMLError:
-            print("Exception when parsing YAML e")
+            print(f"Exception when parsing YAML \n{node_yaml_str}")
             return None
---
+++ models/test_instance_featurizer.py
@@ -0,0 +1,16 @@
+import pytest
+
+from .instance_featurizer import DefaultInstanceFeaturizer
+
+
+MOCK_YAML = """
+cpu_requirement: 2.3
+memory_requirement: 4.5
+network_requirement: 8.7
+"""
+
+
+def test_instance_featurizer():
+    featurizer = DefaultInstanceFeaturizer()
+    result = featurizer(MOCK_YAML)
+    assert result.tolist() == [2.3, 4.5, 8.7]

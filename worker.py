e50b12486a2764e6bdd6eef797eda2c96c0734ab
Sidharth Menon <menon.sid.k@gmail.com>
2021-12-07 19:33:56 -0500

    [structure] docker worker

---
+++ worker.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class WorkerInstance:
+    ## Launches jobs in a subprocess and collects some statistics on them
+    def __init__(self) -> None:
+        pass
+
+    def launch(self, function_key: str, action_key: str) -> List[float]:
+        pass
